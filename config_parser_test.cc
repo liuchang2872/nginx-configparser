833b8fb40826c78766619b67faf06944bcde610d
Yanzun Huang <yanzunhuang@Yanzuns-MacBook-Pro.local>
2017-01-18 16:55:25 -0800

    Fix a bug in the previous fix: empty statement shouldn't be allowed. Also added a testcase to test nested mismatched bracket

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -96,3 +96,10 @@ TEST_F(NginxConfigParserTest, nestedBracketConfig3 ){
   std::stringstream config_stream(config_string);
   EXPECT_TRUE(parser.Parse(&config_stream, &out_config));
 }
+
+// A testcase that combines nested and mismatched brackets
+TEST_F(NginxConfigParserTest, nestedMismatchedBracketConfig ){
+  std::string config_string = "a {b {c;d {e;}} f {g;}";
+  std::stringstream config_stream(config_string);
+  EXPECT_FALSE(parser.Parse(&config_stream, &out_config));
+}
